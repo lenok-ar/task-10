c9ab53ce1bed3fa65081a0a24b188889168e16cf
lenok-ar <leno4ka279a@gmail.com>
2025-05-11 23:04:02 +0700

    init Invalid menu choice exception

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OverLoad;
+using System;
+
+namespace task10
+{
+  [TestClass]
+  public class InvalidMenuChoiceExceptionTests
+  {
+    [TestMethod]
+    public void InvalidMenuChoiceException_DefaultConstructor_InitializesCorrectly()
+    {
+      InvalidMenuChoiceException exception;
+      exception = new InvalidMenuChoiceException();
+
+      Assert.AreEqual("Некорректный выбор в меню", exception.Message);
+      Assert.IsNull(exception.MenuChoice);
+    }
+  }
+}
